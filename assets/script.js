72daa75e44f21f287d1858c3c28da27cc50dfaf1
Jakers811 <akerstjustin@gmail.com>
2021-03-19 23:43:04 -0500

    Changed from js fetch to Jquery AJAX

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,14 @@
-function getApi() {
-  var url = 'https://api.watchmode.com/v1/sources/?apiKey=wPaBpIHLEyhglS4raH1LguFDHh1P1JdoAj6vZCC6';  
-  fetch(url, { method: 'Get'})
-    .then(function (response) {
-      return response.json();
-      // })
-      // .then(function (data) {
-      //   for (var i = 0; i < data.length; i++) {}
-  });
-};
-
 $(document).ready(function(){
   $('.carousel').carousel();
-
-  getApi();
 });
 
+var requestUrl = 'https://api.watchmode.com/v1/sources/?apiKey=wPaBpIHLEyhglS4raH1LguFDHh1P1JdoAj6vZCC6';
+
+// JQuery AJAX
+$.ajax({
+  url: requestUrl,
+  method: 'GET',
+}).then(function (response) {
+  console.log('AJAX Response \n-------------');
+  console.log(response);
+});
