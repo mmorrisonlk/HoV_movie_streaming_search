151269ab55059fe87d6ae23d5fba6678a7b338aa
Jakers811 <akerstjustin@gmail.com>
2021-03-19 23:31:09 -0500

    Added working watchmode API fetch.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,17 @@
-  $(document).ready(function(){
-    $('.carousel').carousel();
+function getApi() {
+  var url = 'https://api.watchmode.com/v1/sources/?apiKey=wPaBpIHLEyhglS4raH1LguFDHh1P1JdoAj6vZCC6';  
+  fetch(url, { method: 'Get'})
+    .then(function (response) {
+      return response.json();
+      // })
+      // .then(function (data) {
+      //   for (var i = 0; i < data.length; i++) {}
   });
+};
+
+$(document).ready(function(){
+  $('.carousel').carousel();
+
+  getApi();
+});
+
