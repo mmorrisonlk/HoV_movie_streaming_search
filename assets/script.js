0a17577101a848bf2cda4d373d60a10fbba4dd3b
Jakers811 <akerstjustin@gmail.com>
2021-03-20 20:31:07 -0500

    Added working carousel with top 10 movies.

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,36 @@
 $(document).ready(function(){
   $('.carousel').carousel();
+  popularMovies();
 });
 
-var requestUrl = 'https://api.watchmode.com/v1/sources/?apiKey=wPaBpIHLEyhglS4raH1LguFDHh1P1JdoAj6vZCC6';
+const searchAPI = "2ae52f27dbb481080bd1f2c839a0d6d7";
 
-// JQuery AJAX
-$.ajax({
-  url: requestUrl,
-  method: 'GET',
-}).then(function (response) {
-  console.log('AJAX Response \n-------------');
-  console.log(response);
-});
+function popularMovies() {
+  const requestUrl = "https://api.themoviedb.org/3/movie/popular?api_key=" + searchAPI + "&language=en-US&page=1";
+
+  $.ajax({
+    url: requestUrl,
+    method: 'GET',
+  }).then(function (response) {
+    // console.log('AJAX Response \n-------------');
+    // console.log(response);
+    for (i = 0; i < 10; i++) {
+        let popularMoviePosters = "https://image.tmdb.org/t/p/w500" + response.results[i].poster_path;
+        // console.log(popularMoviePosters);
+        var banner = document.getElementById("banner");
+        var anchor = document.createElement("a");
+        anchor.classList.add('carousel-item');
+        var image = document.createElement("img");
+        image.src = popularMoviePosters;
+        anchor.appendChild(image);
+        banner.appendChild(anchor);
+    };
+    // remove placeholder image
+    var clean = document.getElementById("clean");
+    clean.parentNode.removeChild(clean);
+
+    // Reinitialize carousel
+    var elems2 = document.querySelectorAll('.carousel');
+    var instances2 = M.Carousel.init(elems2);
+  });
+};
